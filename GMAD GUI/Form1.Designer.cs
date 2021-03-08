e816eac048ace8caccfab188c71ca793618b21ee
CrazyBloo <CrazyBloo@users.noreply.github.com>
2021-03-08 01:33:23 -0500

    Upload

---
+++ GMAD GUI/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace GMAD_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.consoleControl1 = new ConsoleControl.ConsoleControl();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile4 = new MetroFramework.Controls.MetroTile();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTile5 = new MetroFramework.Controls.MetroTile();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // consoleControl1
+            // 
+            this.consoleControl1.IsInputEnabled = false;
+            this.consoleControl1.Location = new System.Drawing.Point(422, 63);
+            this.consoleControl1.Name = "consoleControl1";
+            this.consoleControl1.SendKeyboardCommandsToProcess = false;
+            this.consoleControl1.ShowDiagnostics = false;
+            this.consoleControl1.Size = new System.Drawing.Size(366, 375);
+            this.consoleControl1.TabIndex = 0;
+            this.consoleControl1.Visible = false;
+            this.consoleControl1.Load += new System.EventHandler(this.consoleControl1_Load);
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(188, 319);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(66, 43);
+            this.metroTile1.TabIndex = 2;
+            this.metroTile1.Text = "Extract!";
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroTile3);
+            this.metroPanel1.Controls.Add(this.metroTile2);
+            this.metroPanel1.Controls.Add(this.metroLabel1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(23, 20);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(179, 64);
+            this.metroPanel1.TabIndex = 3;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(68, 25);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(59, 39);
+            this.metroTile3.TabIndex = 4;
+            this.metroTile3.Text = "Extract";
+            this.metroTile3.UseSelectable = true;
+            this.metroTile3.Click += new System.EventHandler(this.metroTile3_Click);
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(3, 25);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(59, 39);
+            this.metroTile2.TabIndex = 4;
+            this.metroTile2.Text = "Create";
+            this.metroTile2.UseSelectable = true;
+            this.metroTile2.Click += new System.EventHandler(this.metroTile2_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(0, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(95, 25);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "GMAD GUI";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroTile4
+            // 
+            this.metroTile4.ActiveControl = null;
+            this.metroTile4.Location = new System.Drawing.Point(23, 115);
+            this.metroTile4.Name = "metroTile4";
+            this.metroTile4.Size = new System.Drawing.Size(62, 39);
+            this.metroTile4.TabIndex = 4;
+            this.metroTile4.Text = "Browse";
+            this.metroTile4.UseSelectable = true;
+            this.metroTile4.Click += new System.EventHandler(this.metroTile4_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(303, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[] {
+        "Select the .gma to extract"};
+            this.metroTextBox1.Location = new System.Drawing.Point(91, 123);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(325, 23);
+            this.metroTextBox1.TabIndex = 5;
+            this.metroTextBox1.Text = "Select the .gma to extract";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTile5
+            // 
+            this.metroTile5.ActiveControl = null;
+            this.metroTile5.Location = new System.Drawing.Point(23, 198);
+            this.metroTile5.Name = "metroTile5";
+            this.metroTile5.Size = new System.Drawing.Size(62, 39);
+            this.metroTile5.TabIndex = 4;
+            this.metroTile5.Text = "Browse";
+            this.metroTile5.UseSelectable = true;
+            this.metroTile5.Click += new System.EventHandler(this.metroTile5_Click);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(303, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[] {
+        "Output destination"};
+            this.metroTextBox2.Location = new System.Drawing.Point(91, 206);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(325, 23);
+            this.metroTextBox2.TabIndex = 5;
+            this.metroTextBox2.Text = "Output destination";
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel2.Location = new System.Drawing.Point(91, 365);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(262, 15);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Depending on the addon size, this may take a while";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroTile5);
+            this.Controls.Add(this.metroTile4);
+            this.Controls.Add(this.metroTile1);
+            this.Controls.Add(this.consoleControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "w";
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ConsoleControl.ConsoleControl consoleControl1;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroTile metroTile4;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTile metroTile5;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
---
+++ GMAD GUI/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace GMAD_GUI
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        //Use this to keep track of wether we are extracting or creating an addon
+        private enum GMADState
+        {
+            Create,
+            Extract
+        }
+        private GMADState gmadState = GMADState.Extract;
+        public Form1()
+        {
+            InitializeComponent();
+            //Set the callback from the console output
+            
+
+        }
+
+        private void consoleControl1_Load(object sender, EventArgs e)
+        {
+            consoleControl1.OnConsoleOutput += ConsoleControl1_OnConsoleOutput;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private string gmaLocation;
+        private string outputLocation;
+        private string addonLocation;
+        //Top browse button, sets .gma to extract or folder to compile
+        private void metroTile4_Click(object sender, EventArgs e)
+        {
+            if (gmadState == GMADState.Extract)
+            {
+                var fileBrowser = new OpenFileDialog();
+                fileBrowser.Multiselect = false;
+                fileBrowser.Filter = "GMOD Addons|*.gma|All Files|*.*";
+                fileBrowser.ShowDialog();
+                metroTextBox1.Text = fileBrowser.FileName;
+                gmaLocation = fileBrowser.FileName;
+            } else
+            {
+                var fileBrowser = new CommonOpenFileDialog();
+                fileBrowser.IsFolderPicker = true;
+                CommonFileDialogResult result = fileBrowser.ShowDialog();
+                metroTextBox1.Text = fileBrowser.FileName;
+                addonLocation = fileBrowser.FileName;
+            }
+        }
+        //Bottom browse button, sets output location
+        private void metroTile5_Click(object sender, EventArgs e)
+        {
+            if (gmadState == GMADState.Extract)
+            {
+                var fileBrowser = new CommonOpenFileDialog();
+                fileBrowser.IsFolderPicker = true;
+                CommonFileDialogResult result = fileBrowser.ShowDialog();
+                metroTextBox2.Text = fileBrowser.FileName;
+                outputLocation = fileBrowser.FileName;
+            } else
+            {
+                var fileBrowser = new SaveFileDialog();
+                fileBrowser.Filter = "GMOD Addons|*.gma|All Files|*.*";
+                fileBrowser.DefaultExt = ".gma";
+                fileBrowser.ShowDialog();
+                metroTextBox2.Text = fileBrowser.FileName;
+                gmaLocation = fileBrowser.FileName;
+            }
+            
+        }
+        // The extract/create button
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            
+            if (gmadState == GMADState.Extract)
+            {
+
+                var startInfo = new ProcessStartInfo();
+                startInfo.FileName = @"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe";
+                startInfo.Arguments = "extract -file " + gmaLocation + " -out " + outputLocation;
+                consoleControl1.Visible = true;
+                consoleControl1.StartProcess(startInfo.FileName, startInfo.Arguments);
+
+            } else {
+
+                var startInfo = new ProcessStartInfo();
+                startInfo.FileName = @"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe";
+                startInfo.Arguments = "create -folder " + addonLocation + " -out " + gmaLocation;
+                consoleControl1.Visible = true;
+                consoleControl1.StartProcess(startInfo.FileName, startInfo.Arguments);
+
+            }  
+        }
+        
+        //This function is called whenever the console receives an output
+        private void ConsoleControl1_OnConsoleOutput(object sender, ConsoleControl.ConsoleEventArgs args)
+        {
+            
+           if (consoleControl1.InternalRichTextBox.Text.Contains("Done!"))
+            {
+
+                consoleControl1.ClearOutput();
+                var result = System.Windows.Forms.MessageBox.Show("Success!", "Finished Extracting");
+                
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    consoleControl1.ClearOutput();
+                    consoleControl1.InternalRichTextBox.Text = "";
+                    consoleControl1.Visible = false;
+                    //Reset arguments
+                    gmaLocation = "";
+                    outputLocation = "";
+                    addonLocation = "";
+                }
+            } 
+            if (consoleControl1.InternalRichTextBox.Text.Contains("Successfully saved"))
+            {
+                consoleControl1.ClearOutput();
+                var result = System.Windows.Forms.MessageBox.Show("Success!", "Addon created");
+               
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    consoleControl1.ClearOutput();
+                    consoleControl1.InternalRichTextBox.Text = "";
+                    consoleControl1.Visible = false;
+                    //Reset arguments
+                    gmaLocation = "";
+                    outputLocation = "";
+                    addonLocation = "";
+                }
+            } 
+
+        }
+        // Switch to the create window
+        private void metroTile2_Click(object sender, EventArgs e)
+        {
+            gmadState = GMADState.Create;
+            metroTile1.Text = "Create!";
+            metroTextBox1.Text = "Select folder with addon files";
+            //Reset arguments
+            gmaLocation = "";
+            outputLocation = "";
+            addonLocation = "";
+        }
+        //Switch to the extract window
+        private void metroTile3_Click(object sender, EventArgs e)
+        {
+            gmadState = GMADState.Extract;
+            metroTile1.Text = "Extract!";
+            metroTextBox1.Text = "Select .gma to extract";
+            //Reset arguments
+            gmaLocation = "";
+            outputLocation = "";
+            addonLocation = "";
+        }
+    }
+}
